4f59e939255cd1e23121b261f51382aa8c00a036
supeii481 <supeii481@gmail.com>
2026-01-08 11:53:48 +0800

    Fix:修復css reset的bug

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia' // 引入 Pinia
 import App from './App.vue'
 import router from './router' // 引入路由設定
 import '@/assets/scss/main.scss' // 引入全域樣式
+import './assets/scss/base/_reset.scss' //引入全域css reset
 
 const app = createApp(App) // 建立 Vue 應用程式實體
 const pinia = createPinia() // 建立 Pinia 全域狀態管理實體
