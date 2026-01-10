b46d1c017678acfa335ef0edee7c0d933f6cf2f7
tom36596263 <tom36596263@gmail.com>
2026-01-10 09:06:45 +0800

    fix:完善snippets

--- src/views/workspace/EditMealPlan.vue
+++ src/views/workspace/EditMealPlan.vue
@@ -2,32 +2,18 @@
 import { ref } from 'vue';
 import DayColumn from '@/components/workspace/mealplan/DayColumn.vue';
 import ColumnTitle from '@/components/workspace/mealplan/ColumnTitle.vue';
-
-const weekDays = ref([
-  { id: 1, name: '週一', date: '11/11' },
-  { id: 2, name: '週二', date: '11/12' },
-  { id: 3, name: '週三', date: '11/13' },
-  { id: 4, name: '週四', date: '11/14' },
-  { id: 5, name: '週五', date: '11/15' },
-  { id: 6, name: '週六', date: '11/16' },
-  { id: 7, name: '週日', date: '11/17' }
-]);
 </script>
 
 <template>
-  <main class="meal-plan-container">
+  <main class="meal-plan-container col-12">
     <ColumnTitle />
-    <DayColumn
-      v-for="day in weekDays"
-      :key="day.id"
-      :date="day.date"
-      :day-name="day.name"
-    />
+    <DayColumn v-for="day in 7" :key="day" />
   </main>
 </template>
 
 <style lang="scss" scoped>
 .meal-plan-container {
   display: flex;
+  gap: 5px;
 }
 </style>
