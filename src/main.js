c129210458a614fa82fedc4700b60d64144b4102
tom36596263 <tom36596263@gmail.com>
2026-01-17 17:10:06 +0800

    fix: 嘗試修復全域引用public內的圖片或資料的路徑

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import router from './router' // 引入路由設定
 import '@/assets/scss/base/_reset.scss' //引入全域css reset
 import '@/assets/scss/main.scss' // 引入全域樣式
 import '@fortawesome/fontawesome-free/css/all.css' // 引入fontawesome
+import { parsePublicFile } from '@/utils/parseFile'// 引入圖片路徑的全域變數
 
 const app = createApp(App) // 建立 Vue 應用程式實體
 const pinia = createPinia() // 建立 Pinia 全域狀態管理實體
 
 app.use(pinia) // 插件：啟用 Pinia 狀態管理
 app.use(router) // 插件：啟用 Vue Router 路由導覽
+app.config.globalProperties.$parsePublicFile = parsePublicFile //設定全域屬性
 app.mount('#app') // 將應用程式掛載至 index.html 的 #app 容器，正式啟動渲染
