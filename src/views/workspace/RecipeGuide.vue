54584a46b0d103591acf3844be12e92719f641ef
tom36596263 <tom36596263@gmail.com>
2026-01-24 12:01:51 +0800

    feat: 開發步驟頁面

--- src/views/workspace/RecipeGuide.vue
+++ src/views/workspace/RecipeGuide.vue
@@ -57,8 +57,9 @@ watch(() => route.params.id, (newId) => {
 const currentStepIndex = ref(0); // 預設顯示第一步 (索引為 0)
 // computed是vue內建的API，從JavaScript的角度來看它也是函式，是vue提供的一套流水線。
 // computed裡面需要放置一段邏輯，不能直接給變數，否則computed只會在網頁載入的當下接收到該變數代表的資料，後續就算該變數有變動computed也不知道要變。
+// currentStepData是變數，也是計算屬性
 const currentStepData = computed(() => allSteps.value[currentStepIndex.value] || {});
-
+// 一般屬性
 const stepNotes = ref({}); // 宣告變數以儲存每個步驟的筆記，格式如 { step_id: '筆記內容' }
 
 
