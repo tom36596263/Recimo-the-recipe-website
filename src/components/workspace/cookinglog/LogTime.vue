2638b158e7b6928c540df9fa9c4d3aa54a109070
tom36596263 <tom36596263@gmail.com>
2026-02-11 15:00:40 +0800

    fix: 修正百分比計算邏輯

--- src/components/workspace/cookinglog/LogTime.vue
+++ src/components/workspace/cookinglog/LogTime.vue
@@ -14,7 +14,7 @@ const isOverTime = computed(() => actualTime.value > estimateTime.value);
 // 3. 計算差異百分比
 const diffPercentage = computed(() => {
     const diff = actualTime.value - estimateTime.value;
-    const percent = (diff / estimateTime.value) * 100;
+    const percent = 100 + (diff / estimateTime.value) * 100;
     if (percent > 0) {
         return `+${percent.toFixed(1)}%`;
     } else {
